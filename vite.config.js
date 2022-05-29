9ed1f5a4c2157a282c9013f573c9a92982d3cb44
namheheh <nambx1234@gmail.com>
2022-05-29 14:06:06 +0700

    tạo các page

---
+++ vite.config.js
@@ -0,0 +1,17 @@
+// vite.config.js
+const { resolve } = require('path')
+const { defineConfig } = require('vite')
+
+module.exports = defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        detail: resolve(__dirname, 'detail.html'),
+        product: resolve(__dirname, 'product.html'),
+
+        
+      }
+    }
+  }
+})
